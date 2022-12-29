8b2459a972f384593b25f55538712e441e0c7896
deatrisya <mirelldee47@gmail.com>
2022-12-29 07:49:20 +0700

    rezise content

--- lib/screens/viewStudent.dart
+++ lib/screens/viewStudent.dart
@@ -17,140 +17,6 @@ class _ViewStudentState extends State<ViewStudent> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: const Text("UAS Mobile"),
-      //   backgroundColor: Colors.teal,
-      // ),
-      // body: Container(
-      //   padding: EdgeInsets.all(16.0),
-      //   child: Column(
-      //     crossAxisAlignment: CrossAxisAlignment.start,
-      //     children: [
-      //       const Center(
-      //         child: Text(
-      //           '\n\nDETAIL BIODATA',
-      //           style: TextStyle(
-      //               fontSize: 50,
-      //               color:  Color(0xFFe80054),
-      //               fontWeight: FontWeight.bold),
-      //         ),
-      //       ),
-      //       const SizedBox(
-      //         height: 50.0,
-      //       ),
-      //       Row(
-      //         children: [
-      //           const Text('NIM        :',
-      //               style: TextStyle(
-      //                   color: Colors.teal,
-      //                   fontSize: 16,
-      //                   fontWeight: FontWeight.w600)),
-      //           Padding(
-      //             padding: const EdgeInsets.only(left: 20),
-      //             child: Text(widget.student.nim ?? '',
-      //                 style: TextStyle(fontSize: 16)),
-      //           ),
-      //         ],
-      //       ),
-      //       const SizedBox(
-      //         height: 20,
-      //       ),
-      //       Row(
-      //         children: [
-      //           const Text('Nama     :',
-      //               style: TextStyle(
-      //                   color: Colors.teal,
-      //                   fontSize: 16,
-      //                   fontWeight: FontWeight.w600)),
-      //           Padding(
-      //             padding: const EdgeInsets.only(left: 20),
-      //             child: Text(widget.student.nama ?? '',
-      //                 style: TextStyle(fontSize: 16)),
-      //           ),
-      //         ],
-      //       ),
-      //       const SizedBox(
-      //         height: 20,
-      //       ),
-      //       Row(
-      //         children: [
-      //           const Text('Alamat   :',
-      //               style: TextStyle(
-      //                   color: Colors.teal,
-      //                   fontSize: 16,
-      //                   fontWeight: FontWeight.w600)),
-      //           Padding(
-      //             padding: const EdgeInsets.only(left: 20),
-      //             child: Text(widget.student.alamat ?? '',
-      //                 style: TextStyle(fontSize: 16)),
-      //           ),
-      //         ],
-      //       ),
-      //       const SizedBox(
-      //         height: 20,
-      //       ),
-      //       Row(
-      //         children: [
-      //           const Text('Telepon  :',
-      //               style: TextStyle(
-      //                   color: Colors.teal,
-      //                   fontSize: 16,
-      //                   fontWeight: FontWeight.w600)),
-      //           Padding(
-      //             padding: const EdgeInsets.only(left: 20),
-      //             child: Text(widget.student.telepon ?? '',
-      //                 style: TextStyle(fontSize: 16)),
-      //           ),
-      //         ],
-      //       ),
-      //       const SizedBox(
-      //         height: 20,
-      //       ),
-      //       Row(
-      //         children: [
-      //           const Text('Gender   :',
-      //               style: TextStyle(
-      //                   color: Colors.teal,
-      //                   fontSize: 16,
-      //                   fontWeight: FontWeight.w600)),
-      //           Padding(
-      //             padding: const EdgeInsets.only(left: 20),
-      //             child: Text(widget.student.gender ?? '',
-      //                 style: TextStyle(fontSize: 16)),
-      //           ),
-      //         ],
-      //       ),
-      //        const SizedBox(
-      //         height: 20,
-      //       ),
-      //       Row(
-      //         children: [
-      //           const Text('Foto        :',
-      //               style: TextStyle(
-      //                   color: Colors.teal,
-      //                   fontSize: 16,
-      //                   fontWeight: FontWeight.w600)),
-      //           Padding(
-      //             padding: const EdgeInsets.only(left: 5),
-      //             child:
-      //             widget.student.image != ''
-      //             ? Container(
-      //               width: 200,
-      //               height: 200,
-      //                 child: Image.file(
-      //                   File(widget.student.image ?? ''),
-
-      //                 ),
-      
-      //               )
-      //             : Container(),
-      //           ),
-      //         ],
-      //       ),
-      //     ],
-      //   ),
-      // ),
-      // resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       body: Column(
         children: [
@@ -192,32 +58,110 @@ class _ViewStudentState extends State<ViewStudent> {
             height: MediaQuery.of(context).size.height * 0.05,
           ),
           Container(
-            height: 500,
+            clipBehavior: Clip.none,
             child: Stack(
+              clipBehavior: Clip.none,
               children: [
-                Positioned(
-                    top: 25,
-                    left: 20,
-                    child: Material(
-                      child: Container(
-                        height: 150.0,
-                        width: MediaQuery.of(context).size.width * 0.9,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(0.0),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.3),
-                              offset: Offset(-5.0, 10.0),
-                              blurRadius: 20.0,
-                              spreadRadius: 4.0,
-                            )
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 12,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        offset: Offset(-5.0, 10.0),
+                        blurRadius: 20.0,
+                        spreadRadius: 4.0,
+                      )
+                    ],
+                  ),
+                  padding: EdgeInsets.only(
+                    left: 145,
+                    right: 12,
+                    bottom: 30,
+                    top: 30,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text('NIM        : ',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600)),
+                            Expanded(
+                              child: Text(widget.student.nim ?? '',
+                                  style: TextStyle(fontSize: 16)),
+                            ),
                           ],
                         ),
                       ),
-                    )),
+                      Container(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text('Nama     : ',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600)),
+                            Expanded(
+                              child: Text(widget.student.nama ?? '',
+                                  style: TextStyle(fontSize: 16)),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Alamat   : ',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                            Expanded(
+                              child: Text(
+                                widget.student.alamat ?? '',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text('Gender   : ',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600)),
+                            Expanded(
+                              child: Text(widget.student.gender ?? '',
+                                  style: TextStyle(fontSize: 16)),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
                 Positioned(
-                  top: 0,
+                  top: -30,
                   left: 30,
                   child: Card(
                     elevation: 10.0,
@@ -226,77 +170,20 @@ class _ViewStudentState extends State<ViewStudent> {
                       borderRadius: BorderRadius.circular(15.0),
                     ),
                     child: Container(
-                        height: 150,
-                        width: 100,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10.0),
-                          image: DecorationImage(
-                              fit: BoxFit.fill,
-                              image:
-                                  FileImage(File(widget.student.image ?? ''))),
-                        )),
+                      height: 150,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        image: DecorationImage(
+                          fit: BoxFit.fill,
+                          image: FileImage(
+                            File(widget.student.image ?? ''),
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-                Positioned(
-                  top: 45,
-                  left: 130,
-                  child: Container(
-                      height: 100,
-                      width: 250,
-                      padding: EdgeInsets.only(left: 15, bottom: 20, top: 10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: [
-                              const Text('NIM        : ',
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w600)),
-                              Text(widget.student.nim ?? '',
-                                  style: TextStyle(fontSize: 16)),
-                            
-                            ],
-                          ),
-                           
-                          Row(
-                            children: [
-                              const Text('Nama     : ',
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w600)),
-                              Text(widget.student.nama ?? '',
-                                  style: TextStyle(fontSize: 16)),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              const Text('Alamat   : ',
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w600)),
-                              Text(widget.student.alamat ?? '',
-                                  style: TextStyle(fontSize: 16)),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              const Text('Gender   : ',
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w600)),
-                              Text(widget.student.gender ?? '',
-                                  style: TextStyle(fontSize: 16)),
-                            ],
-                          ),
-                        ],
-                      )
-                      ),
-                )
               ],
             ),
           ),
@@ -304,8 +191,8 @@ class _ViewStudentState extends State<ViewStudent> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => MyHomePage()))
+          Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => MyHomePage()))
               .then((data) {});
         },
         child: const Icon(Icons.arrow_back),
